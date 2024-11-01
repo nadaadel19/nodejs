a2ecc9db5d259a38a6e96919ddb6efb50138186b
unknown <engnada.adel19@gmail.com>
2024-11-01 15:46:53 +0200

    m1

---
+++ root.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const PORT  = 4008
+const HOST = '0.0.0.0'
+
+// post get put delete 
+
+app.get('/', (req, res)=>{
+    res.send("hello to our image !")
+
+})
+app.get('/', (req, res)=>{
+    res.send("hello to our image !")
+
+})
+
+app.listen(PORT, HOST, ()=>{
+    console.log("application")
+} )
+
